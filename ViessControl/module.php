bb939c5eaa8af0cfec3333a83d086de6702e98b4
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2018-01-31 22:53:07 +0100

    Update module.php

--- ViessControl/module.php
+++ ViessControl/module.php
@@ -238,7 +238,7 @@
           if ( $this->startCommunication() === true ) {
             // Init successful
 		  
-	    $result = getDataFromControl( "00F8", 2 );	  
+	    $result = $this->getDataFromControl( "00F8", 2 );	  
 		  
             // send command to request identification data from control ( 0x41 0x05 0x00 0x01 0x00 0xF8 0x02 0x00 ) (Protocol 300)
             //if ( $this->GetBuffer( "PortState" ) == ViessControl::COMPORT_READY )
